879de2672d9945ddadc9af922a377c24ad3b9075
Bruno_Barros <barrosbrunofelipe@gmail.com>
2022-07-31 17:37:09 -0300

    feat: testes e2e página top e validando quantidade de recomendações exibidas em tela

--- cypress/e2e/top.cy.js
+++ cypress/e2e/top.cy.js
@@ -1,7 +1,14 @@
 describe("Top page tests", () => {
-    it("must list 10 recommendations in descending order", () => {
+    it("10 recommendations should appear on the display", () => {
         cy.visit("http://localhost:3000/");
         cy.contains("Top").click();
+        cy.get("article").then(($recommendations) => {
+            expect($recommendations).to.have.length.of.at.most(10);
+          });
+        cy.end();
+    });
+
+    it("must list 10 recommendations in descending order", () => {
         cy.get("#root article:first div:nth-child(2) div:first").click();
         cy.get("#root article:first div:last").should("have.text", "50");
         cy.get("#root article:last div:last").should("have.text", "9");
